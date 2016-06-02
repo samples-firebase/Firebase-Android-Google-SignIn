988780482eef2293f7086f3e40573de1d1dff447
Tushar Rai <Tushar Rai>
2016-06-02 17:05:14 +0530

    Initial commit

---
+++ app/src/main/java/com/android/www/helloworld/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.android.www.helloworld;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tushar Rai on 6/2/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hideProgressDialog();
+    }
+
+}
